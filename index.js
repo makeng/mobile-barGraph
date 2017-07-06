5cb9ba5b400c41b994b042ca4211b30fa70b0e39
mzk <frontken@sina.com>
2017-07-06 10:53:35 +0800

    新增30天数据表和y轴

--- index.js
+++ index.js
@@ -2,52 +2,76 @@
  * Created by Administrator on 2017/7/3.
  */
 //步数列表
-var stepList = {
-	step: [5900, 5000, 3000, 2563, 125, 22521, 9632],	//字符串
+var stepList7days = {
+	step: [5900, 5000, 3000, 2563, 125, 22521, 9632],	//可以是字符串
 	barHeight: [],
 	stepTop: 20000,
-	showingStepIdx: 6,	//显示步数的index
-	todayStep: 621,	//今天步数
-	date: [24, 25, 26, 27, 28, 29, 30]	//字符串
+	date: [24, 25, 26, 27, 28, 29, 30]	//可以是字符串
+};
+var stepList30days = {
+	step: [15900, 5000, 3000, 2563, 125, 22521, 0,
+		5900, 6321, 3000, 2563, 125, 4217, 0,
+		4586, 5000, 3000, 2563, 5621, 22521, 0,
+		5900, 1256, 3000, 2563, 125, 3652, 0,
+		5900, 22222
+	],
+	barHeight: [],
+	stepTop: 20000,
+	date: ['', 2, '','','','','',
+		'', '', 9,'','','','',
+		'', '', 16,'','','','',
+		'', '', 23,'','','','',
+		'', 30
+	]
 };
 /*	设置图表
+*	@param $el对应dom对象 stepList步数对象， barWidthPercent平分后柱子占据包裹层的宽度百分比
  * */
-function setGraph($el) {
-	var $step = $el.find('.graph-content-bars em');
-	var $barWrap = $el.find('.graph-content-bars li');
-	var $bar = $el.find('.graph-content-bars i');
-	var $date = $el.find('.graph-date li');
+function setGraph($el, stepList, barWidthPercent) {
+	var $bars = $el.find('.graph-content-bars');
+	var $barWrap = $bars.find('li');
+	var $dates = $el.find('.graph-date');
+	//根据步数数量，控制柱子宽度，平分
+	var width = 100 / (stepList.step.length) + '%';
 	//根据步数循环,产生数据
 	stepList.step.forEach(function (item, idx) {
-		if ( item > 20000 ){	//高度限制
-			stepList.barHeight[idx] = 100;
-		}else{
-			stepList.barHeight[idx] = item * 100 / stepList.stepTop;
+		var stepNum = parseInt(item);
+		if (stepNum > 20000) {	//高度限制
+			stepList.barHeight[idx] = '100%';
+		} else {
+			stepList.barHeight[idx] = stepNum * 100 / stepList.stepTop + '%';
 		}
-		//设置bar高度
-		$bar.eq(idx).css({
-			height: stepList.barHeight[idx] + '%'
-		});
-		console.log(stepList.barHeight[idx] + '%');
-		//设置步数
-		$step.eq(idx).text(item);
+		var barStr = '<i style="height:' +
+			stepList.barHeight[idx] + ';width:' + barWidthPercent + '%;left:' + ((100-barWidthPercent)/2) + '%' +
+			'"></i>';
+		var stepStr = '<em>' + stepNum + '</em>';
+		$bars.append('<li onclick="barWrapClick(' + $el.index() + ',' + idx + ')" style="width:' + width + '">' +
+			barStr + stepStr +
+			'<b></b></li>');	//添加元素，带点击事件
 		//日期
-		$date.eq(idx).text(stepList.date[idx]);
-	});
-	/*	事件绑定:点击柱子移动步数位置
-	* */
-	$barWrap.click(function () {
-		$(this).find('em').css({
-			top: '2%',
-		});
-		$(this).siblings('li').find('em').css({
-			top: '100%'
-		});
+		$dates.append('<li style="width:' + width + '">' + stepList.date[idx] + '</li>');
 	});
 }
 
+/*	事件绑定:点击柱子移动步数位置
+* 	@param graphIdx对应表格的序号 liIdx对应柱子包裹层的序号
+ * */
+function barWrapClick(graphIdx, liIdx) {
+	var $this = $('.graph').eq(graphIdx).find('.graph-content-bars li').eq(liIdx);
+	var $siblings = $this.siblings('li');
+	$this.find('em').css({
+		top: '2%',
+		opacity: 1
+	});
+	$this.find('b').show();
+	$siblings.find('em').css({
+		top: '100%',
+		opacity: 0
+	});
+	$siblings.find('b').hide();
+}
+
 $(function () {
-	setTimeout(function () {	//延时为了查看动画效果
-		setGraph($('.graph'));
-	}, 500);
+	setGraph($('.graph').eq(0), stepList7days, 20);
+	setGraph($('.graph').eq(1), stepList30days, 75);
 });
